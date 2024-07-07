515dfcfa5abb820f9d8b1696ebffa3c49d2a1d48
KribbyWare <162795537+KribbyWare@users.noreply.github.com>
2024-07-07 15:01:19 +0200

    source.lua

--- source.lua
+++ source.lua
@@ -58,8 +58,8 @@ local themes = {
 local themeStyles = {
     DarkTheme = {
         SchemeColor = Color3.fromRGB(64, 64, 64),
-        Background = Color3.fromRGB(0, 0, 0),
-        Header = Color3.fromRGB(22, 22, 22),
+        Background = Color3.fromRGB(35, 35, 35),
+        Header = Color3.fromRGB(60, 60, 60),
         TextColor = Color3.fromRGB(255,255,255),
         ElementColor = Color3.fromRGB(20, 20, 20)
     },
