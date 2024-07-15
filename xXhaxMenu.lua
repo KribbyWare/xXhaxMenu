c36bc09d7a922374b4d6d2288c92867394ff157b
KribbyWare <162795537+KribbyWare@users.noreply.github.com>
2024-07-15 16:39:55 +0200

    xXhaxMenu.lua

--- xXhaxMenu.lua
+++ xXhaxMenu.lua
@@ -1,11 +1,18 @@
+local xXhaxColors = {
+    SchemeColor = Color3.fromRGB(255,0,0),
+    Background = Color3.fromRGB(255, 255, 255),
+    Header = Color3.fromRGB(250, 250, 250),
+    TextColor = Color3.fromRGB(0,0,0),
+    ElementColor = Color3.fromRGB(235, 235, 235)
+}
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/KribbyWare/xXhaxMenu/main/source.lua"))()
-local Window = Library.CreateLib("xXhax Menu", "DarkTheme")
+local Window = Library.CreateLib("xXhax Menu v2.0", xXhaxColors)
  
 game:GetService("StarterGui"):SetCore("SendNotification", { 
 	Title = "xXhax Menu Activated!";
 	Text = "New xXhax Menu v2.0";
-	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 5;
+	Icon = "rbxthumb://type=Asset&id=18488978398&w=150&h=150"})
+Duration = 1.5;
 
 local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
@@ -19,11 +26,22 @@ MainSection:NewSlider("Jump Power", "Change Player Jump Power", 1000, 0, functio
 end)
 
 MainSection:NewSlider("Jump Height", "Change Player Jump Height", 1000, 0, function(ws)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = ws
+    game.Players.LocalPlayer.Character.Humanoid.JumpHeight = ws
 end)
 
-MainSection:NewSlider("Health", "Change Player Health", 100, 0, function(ws)
+MainSection:NewSlider("Health", "Change Player Health", 100, 10, function(ws)
     game.Players.LocalPlayer.Character.Humanoid.Health = ws
+end)
+
+MainSection:NewTextBox("Teleport To Player", "Teleport To Player By Name", function(ws)
+    local p1 = game.Players.LocalPlayer.Character.HumanoidRootPart
+    local p2 = ws
+    local pos = p1.CFrame
+
+    p1.CFrame = game.Players[p2].Character.HumanoidRootPart.CFrame
+end)
+
+MainSection:NewTextBox("Kill Player", "Kill Player By Name", function(ws)
 end)
 
 local Dex = Window:NewTab("Dark DeX")
@@ -36,10 +54,17 @@ end)
 local FlyM = Window:NewTab("Fly Tab")
 local FlyMSection = FlyM:NewSection("Fly Tab")
 
-FlyMSection:NewButton("Fly Activate", "Fly Mode Activate Button", function()
-
+FlyMSection:NewButton("Fly Gui V3: Activate", "Fly Gui V3 Activate Button", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KribbyWare/xXhaxMenu/main/FlyGuiV3.lua'))()
 end)
 
-FlyMSection:NewButton("NoClip Activate", "NoClip Mode Activate Button", function()
-    
+local LightM = Window:NewTab("Lightning")
+local LightMSection = LightM:NewSection("Lightning")
+
+LightMSection:NewSlider("Brightness", "Brightness Change Slider", 100, 0, function(ws)
+    game.Lighting.Brightness = ws
+end)
+
+LightMSection:NewSlider("Clock Time", "Clock Time Change Slider", 24, 0, function(ws)
+    game.Lighting.ClockTime = ws
 end)
